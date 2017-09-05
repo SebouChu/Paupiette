b97cfc42c063acada9418f8d4d3d5ee93ce9b9dc
jonahalmn <jonah.allemonne@icloud.com>
2017-09-05 17:13:17 +0200

    color correct

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< Updated upstream
 ActiveRecord::Schema.define(version: 20170905150043) do
-=======
-ActiveRecord::Schema.define(version: 20170905143815) do
->>>>>>> Stashed changes
 
   create_table "repas", force: :cascade do |t|
     t.string   "titre"
@@ -23,7 +19,6 @@ ActiveRecord::Schema.define(version: 20170905143815) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
-<<<<<<< Updated upstream
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -41,7 +36,5 @@ ActiveRecord::Schema.define(version: 20170905143815) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-=======
->>>>>>> Stashed changes
 
 end
